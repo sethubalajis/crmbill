9bec495b2682c049d3c97376fc037d703e518778
Admin <Admin@local>
2026-01-27 05:21:40 +0530

    Add State model (name, country_id), migration with foreign key, and Filament resource

---
+++ app/Filament/Resources/States/Schemas/StateForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\States\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class StateForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Select::make('country_id')
+                    ->relationship('country', 'name')
+                    ->required(),
+            ]);
+    }
+}

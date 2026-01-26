8c5e5d1295758d9046c9c635742879a1a4c4dcb4
Admin <Admin@local>
2026-01-27 05:24:48 +0530

    Add City model (name, country_id, state_id), migration with foreign keys, and Filament resource

---
+++ app/Filament/Resources/Cities/Schemas/CityForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Cities\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CityForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Select::make('country_id')
+                    ->relationship('country', 'name')
+                    ->required(),
+                Select::make('state_id')
+                    ->relationship('state', 'name')
+                    ->required(),
+            ]);
+    }
+}

8cbae9e95f3795c316319e578289901ef4a6e3d9
Admin <Admin@local>
2026-01-29 05:04:04 +0530

    Fix undefined company variable in quotation view route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Company;
 use App\Models\Quotation;
 
 Route::get('/', function () {
@@ -8,5 +9,10 @@ Route::get('/', function () {
 });
 
 Route::get('/quotations/{quotation}/view', function (Quotation $quotation) {
-    return view('filament.resources.quotations.pages.view-quotation', ['quotation' => $quotation]);
+    $quotation->load('quotationitems.item.gst', 'quotationitems.gst', 'customer');
+    $company = Company::with(['country', 'state', 'city'])->first();
+    return view('filament.resources.quotations.pages.view-quotation', [
+        'quotation' => $quotation,
+        'company' => $company
+    ]);
 })->name('quotations.view');
